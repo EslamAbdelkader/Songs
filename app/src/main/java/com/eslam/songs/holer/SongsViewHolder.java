7c7bbb6b168acf46845d422b277eab21863c0d88
EslamAbdelkader <Islam.Abdelkader@Vodafone.com>
2018-09-21 17:28:52 +0200

    using Glide

--- app/src/main/java/com/eslam/songs/holer/SongsViewHolder.java
+++ app/src/main/java/com/eslam/songs/holer/SongsViewHolder.java
@@ -4,23 +4,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.eslam.songs.R;
-import com.eslam.songs.model.Song;
 import com.eslam.songs.activity.DetailsActivity;
+import com.eslam.songs.model.Song;
 
 public class SongsViewHolder extends RecyclerView.ViewHolder {
 
     private final TextView title;
     private final TextView type;
     private final TextView artist;
+    private final ImageView imageView;
 
     public SongsViewHolder(View itemView) {
         super(itemView);
         title = itemView.findViewById(R.id.title);
         type = itemView.findViewById(R.id.type);
         artist = itemView.findViewById(R.id.artist);
+        imageView = itemView.findViewById(R.id.imageView);
     }
 
     public void bind(final Song item, final Context context) {
@@ -35,5 +40,9 @@ public class SongsViewHolder extends RecyclerView.ViewHolder {
                 context.startActivity(intent);
             }
         });
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder);
+        Glide.with(itemView).load("http:"+item.getImageUrl()).apply(requestOptions).into(imageView);
     }
 }
